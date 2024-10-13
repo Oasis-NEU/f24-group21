dd228e39778b2512d7b62bbcc9ceaed227eae042
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-10-13 02:39:13 -0400

    readjusting express initializing

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const express = require('express');
+
+
 const corsOptions = {
     origin: ["http://localhost:5173", "http://localhost:5174"],
 };
 const next=require("next");
 
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
+
 
 
 // When a GET request is made to the "/api" route, execute this callback function

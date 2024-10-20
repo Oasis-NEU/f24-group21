57d7fa499220393eb756750cc8a689a1ae7aa53f
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-10-20 17:19:01 -0400

    Added getDetails method that returns user information as a JSON format

--- Backend/Data Modules/user.js
+++ Backend/Data Modules/user.js
@@ -15,6 +15,20 @@ class User {
         this.birthday = birthday
         this.password = password
         this.gender = gender
+    
     }
+
+    getDetails(){
+        return{
+            id: this.id,
+            email: this.email,
+            first_name: this.first_name,
+            last_name: this.last_name,
+            birthday: this.birthday,
+            gender: this.gender,
+        }
+    }
+
 }
+
 

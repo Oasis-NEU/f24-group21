3cb18d965ae72de190f5ba22f1fc60a8bec5a39a
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-10-20 18:12:24 -0400

    removed uncessary clientSupabse code (for some reason it ended up here?)

--- Backend/Data Modules/user.js
+++ Backend/Data Modules/user.js
@@ -1,11 +1,3 @@
-const { createClient } = require('@supabase/supabase-js')
-
-const supabaseUrl = 'https://yhnhecvvetphydwjisvi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlobmhlY3Z2ZXRwaHlkd2ppc3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgyMzE1NjIsImV4cCI6MjA0MzgwNzU2Mn0.XS0okew_BZeB224XDsNsCqvrC6OusdNejV3TS_5lGeQ';
-const supabase = createClient(supabaseURL, supabaseKey);
-
-module.exports = supabase;
-
 class User {
     constructor(id, created_at, email, first_name, last_name, birthday, password, gender) {
         this.id = id
@@ -30,5 +22,7 @@ class User {
     }
 
 }
+
+export default User;
 
 
